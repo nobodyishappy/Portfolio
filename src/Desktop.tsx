1e2fa1195a94376faeb79a3eff33803bdea05d4a
nobodyishappy <53548686+nobodyishappy@users.noreply.github.com>
2026-01-19 23:57:13 +0800

    Implement basic desktop layout

---
+++ src/Desktop.tsx
@@ -0,0 +1,44 @@
+import './Desktop.css'
+
+function Desktop() {
+  return (
+    <div id="desktop">
+      <div id="workspace">
+        <div className="icon-box">
+          1
+        </div>
+        <div className="icon-box">
+          2
+        </div>
+        <div className="icon-box">
+          3
+        </div>
+        <div className="icon-box">
+          4
+        </div>
+        <div className="icon-box">
+          5
+        </div>
+      </div>
+      <div id="taskbar">
+        <div className="icon-box">
+          1
+        </div>
+        <div className="icon-box">
+          2
+        </div>
+        <div className="icon-box">
+          3
+        </div>
+        <div className="icon-box">
+          4
+        </div>
+        <div className="icon-box">
+          5
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop
